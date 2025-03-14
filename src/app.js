a55a6cdc3dd2b6166072e09bae2b637a79d54f81
Arman hansda <armanhansda46@gmail.com>
2025-03-15 00:05:19 +0530

    add_controlllers_and_routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors';
 const app = express();
-import cookieParser from cookieParser
+import cookieParser from 'cookie-parser'
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -16,5 +16,12 @@ app.use(express.urlencoded({extended: true, limit: '16kb'})) //extended is use f
 app.use(express.static("public"))// sometimmes we wnt to store file images data inthe server so that it can accessable
 app.use(cookieParser())
 // to accept file to express is not ready so we use multer(therd party package)
+
+
+//import routes
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
